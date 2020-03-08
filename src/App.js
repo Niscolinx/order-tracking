cf758ed98c8f9273063a119dc8fb6f012778fa2b
Niscolinx <munisco12@gmail.com>
2020-03-09 00:40:41 +0100

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <p>Hello world, let's build something amazing</p>
     </div>
   );
