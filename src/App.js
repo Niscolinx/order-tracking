ba743c3f36c97bd761d51268438ee060d8646dab
Niscolinx <munisco12@gmail.com>
2020-03-09 00:42:42 +0100

    updated the App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="">
       <p>Hello world, let's build something amazing</p>
+      <p>Hello world, let's build something amazing</p>
     </div>
   );
 }
