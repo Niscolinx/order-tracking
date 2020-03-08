466c55ea1299cc400a80dba6c12ebcfe7da048ee
Niscolinx <munisco12@gmail.com>
2020-03-08 09:16:41 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <p>Hello world, let's build something amazing</p>
     </div>
   );
 }
