4751f5f9209ef380e80eeb1128c592f0f05b9f16
Niscolinx <munisco12@gmail.com>
2020-03-11 22:24:52 +0100

    Added the form button

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import React from 'react';
 function App() {
   return (
     <div className="main">
+      <h1>Track Your Order</h1>
       <form className = 'order-form'>
         <input type='text' placeholder = 'tracking number'></input>
+        <button>Check</button>
       </form>
     </div>
   );
