67a34dacc26fc043538b92e57fd68319a60e5231
Niscolinx <munisco12@gmail.com>
2020-03-11 22:23:57 +0100

    Styled the headers

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 function App() {
   return (
     <div className="main">
-      <h1>Track Your Order</h1>
       <form className = 'order-form'>
         <input type='text' placeholder = 'tracking number'></input>
       </form>
