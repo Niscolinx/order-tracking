94c7341d4c9d77efe745ba1df4b559857c20bf65
Niscolinx <munisco12@gmail.com>
2020-03-11 22:23:47 +0100

    Styled the headers

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="main">
       <h1>Track Your Order</h1>
-      <h1>Track Your Order</h1>
       <form className = 'order-form'>
         <input type='text' placeholder = 'tracking number'></input>
       </form>
