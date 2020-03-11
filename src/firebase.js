d213257dbf16f260b51a72c9f8d7198630361c0a
Niscolinx <munisco12@gmail.com>
2020-03-11 22:35:32 +0100

    update

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+< !--The core Firebase JS SDK is always required and must be listed first-- >
+    <script src="https://www.gstatic.com/firebasejs/7.10.0/firebase-app.js"></script>
+
+    <!--TODO: Add SDKs for Firebase products that you want to use
+https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.10.0/firebase-analytics.js"></script>
+
+    <script>
+        // Your web app's Firebase configuration
+        var firebaseConfig = {
+            apiKey: "AIzaSyDG-HnTjiQVfgQ42dxkslbXaBoYSBfpEJg",
+        authDomain: "order-tracking-cca8a.firebaseapp.com",
+        databaseURL: "https://order-tracking-cca8a.firebaseio.com",
+        projectId: "order-tracking-cca8a",
+        storageBucket: "order-tracking-cca8a.appspot.com",
+        messagingSenderId: "512946789391",
+        appId: "1:512946789391:web:0a8186f6dbe405cbe2577a",
+        measurementId: "G-M8BSDG0D78"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      firebase.analytics();
+</script>
