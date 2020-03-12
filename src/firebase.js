53704616b90bdfc5ba965ca547315d255ef0a796
Niscolinx <munisco12@gmail.com>
2020-03-12 21:26:36 +0100

    setting up firebase

--- src/firebase.js
+++ src/firebase.js
@@ -17,6 +17,5 @@ firebase.initializeApp(firebaseConfig);
 firebase.initializeApp(firebaseConfig);
 firebase.initializeApp(firebaseConfig);
 firebase.initializeApp(firebaseConfig);
-firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
