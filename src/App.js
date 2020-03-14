42060df82d76cde55d7c7e87370014857b04ab02
Niscolinx <munisco12@gmail.com>
2020-03-14 21:25:33 +0100

    updated the heading

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ function App() {
   return (
     <div className="main">
       <h1>Track Your Order</h1>
+      <h1>Track Your Order</h1>
+      <h1>Track Your Order</h1>
+      <h1>Track Your Order</h1>
+      <h1>Track Your Order</h1>
+      <h1>Track Your Order</h1>
+      <h1>Track Your Order</h1>
+      <h1>Track Your Order</h1>
+      <h1>Track Your Order</h1>
+      <h1>Track Your Order</h1>
+      <h1>Track Your Order</h1>
       <form className = 'order-form'>
         <input type='text' placeholder = 'tracking number'></input>
         <button>Check</button>
