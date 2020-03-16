3b6fb72adf27912505c4fa426758ec9777fbcfe3
Niscolinx <munisco12@gmail.com>
2020-03-16 22:26:05 +0100

    Added the input state

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import * as firebase from 'firebase/app'
 class App extends Component {
 
   state = {
-    trackingValue : ''
+    trackingValue: ''
   }
 
+  onChangeTrackingVal = (e) => {
+    e.preventDefault
+  }
   render() {
- 
-    
+
+
     return (
       <div className="main">
         <h1>Track Your Order</h1>
         <form className='order-form'>
-          <input type='text' placeholder='tracking number' value = {this.state.trackingValue}></input>
+          <input type='text' placeholder='tracking number'
+           value={this.state.trackingValue}
+           onChange={e => this.onChangeTrackingVal(e)}>
+
+           </input>
           <button>Check</button>
         </form>
       </div>
