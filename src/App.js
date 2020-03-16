4389198736d0c3c5b4ccd4c77432ecc9e4016cb7
Niscolinx <munisco12@gmail.com>
2020-03-16 22:17:07 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import * as firebase from 'firebase/app'
 
-class App extends Component{
+class App extends Component {
 
-  render(){
+  render() {
+
     
-
-
+    
+    return (
+      <div className="main">
+        <h1>Track Your Order</h1>
+        <form className='order-form'>
+          <input type='text' placeholder='tracking number'></input>
+          <button>Check</button>
+        </form>
+      </div>
+    );
   }
-      return (
-        <div className="main">
-          <h1>Track Your Order</h1>
-          <form className = 'order-form'>
-            <input type='text' placeholder = 'tracking number'></input>
-            <button>Check</button>
-          </form>
-        </div>
-      );
 }
 
 export default App;
